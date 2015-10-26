4374d6adaf13d40a776da97d2a86a5a82bb28ecd
Thomas Gfuellner <Thomas.Gfuellner@gmail.com>
2015-10-26 17:06:54 +0100

    Spieler Liste wieder als Parameter

--- ttSchweizer.py
+++ ttSchweizer.py
@@ -528,11 +528,10 @@ class RoundInit(Round):
 def getFileNameOfRound(numberOfRound):
     return "runde-%d.tts" % numberOfRound
 
-def getRounds():
+def getRounds(allPlayers):
     """ Schaut nach welche Files vorhanden sind.
         Erzeugt entsprechende Round Instanzen
     """
-    allPlayers = Spieler_Collection()
     roundList = [RoundInit(allPlayers)]
     for i in range(1, 1+NUMBER_OfRounds):
         if os.path.isfile(getFileNameOfRound(i)):
@@ -545,7 +544,8 @@ def getRounds():
 
 if __name__ == '__main__':
 
-    rounds = getRounds()
+    alleSpieler = Spieler_Collection()
+    rounds = getRounds(alleSpieler)
 
     if rounds[-1].isComplete():
         rounds[-1].createStartOfNextRound()
