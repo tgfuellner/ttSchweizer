e39fa8681a92d8d9a3294e1088eb4ac762fd4709
Thomas Gfuellner <Thomas.Gfuellner@gmail.com>
2015-10-26 17:15:28 +0100

    Ranking über Http Request

--- ttSchweizerHttp.py
+++ ttSchweizerHttp.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-from ttSchweizer import *
+from ttSchweizer import getRounds, Spieler_Collection
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!\n"
+def main():
+    alleSpieler = Spieler_Collection()
+    rounds = getRounds(alleSpieler)
+    return str(alleSpieler.getRanking()) + "\n"
 
 if __name__ == "__main__":
     app.run()
