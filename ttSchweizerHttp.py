0539b6b9f511048862777de37281e080f06692f5
Thomas Gfuellner <Thomas.Gfuellner@gmail.com>
2015-11-21 17:06:22 +0100

    Start of Session handling

--- ttSchweizerHttp.py
+++ ttSchweizerHttp.py
@@ -2,20 +2,27 @@
 # -*- coding: UTF-8 -*-
 
 import os
+from uuid import uuid4
 from ttSchweizer import getRounds, Spieler_Collection
-from flask import Flask, render_template, flash, send_from_directory
+from flask import Flask, session, render_template, flash, send_from_directory
+from flask import redirect, url_for
 import ttSchweizer
 
 ttSchweizer.message = flash
 
 app = Flask(__name__)
-app.secret_key = 'FrodoMimi!/Baum'
+app.debug = True
+app.secret_key = 'F1r4o6doM%imi!/Baum'
 
 
 @app.route("/")
 def main():
     alleSpieler = Spieler_Collection()
     rounds = getRounds(alleSpieler)
+    if len(rounds) == 1:
+        session.clear()
+        return redirect(url_for('new'))
+
     ranking = alleSpieler.getRanking()
     rankedSpieler = [sub[0] for sub in ranking]
     thereAreFreilose = (len([s for s in rankedSpieler if s.hatteFreilos]) > 0)
@@ -29,11 +36,16 @@ def main():
                            spielerList=rankedSpieler, thereAreFreilose=thereAreFreilose)
 
 
+@app.route("/new")
+def new():
+    if 'id' not in session:
+        session['id'] = uuid4()
+    return "id = "+ str(session['id'])
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
-    app.debug = True
     app.run()
